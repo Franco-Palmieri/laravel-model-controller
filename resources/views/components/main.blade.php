129c9f74e3756027cecb41bc678241822c47c7c5
Ncesecrede <francescopm95@gmail.com>
2021-09-09 16:36:41 +0100

    Movies

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -3,10 +3,12 @@
         @foreach($allMovies as $movie)
             <div class="card-movie">
                 <div class="foto-container">
-                    <img src="{{$movie->cover}}" alt="">
+                    <div class="hover-ciao">ciao ciao ciao</div>
+                    <div class="cover">
+                        <img src="{{$movie->cover}}" alt="">
+                    </div>
                 </div>
                 <h2 class="title">{{$movie->title}}</h2>
-
             </div>
         @endforeach
     </div>
